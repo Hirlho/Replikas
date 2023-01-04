2975920f024f61fd097823feb41ce129cb4d7d15
GaspardCulis <gaspard.culis@gmail.com>
2023-01-04 17:55:23 +0100

    Youpi

--- tests-playwright/example.spec.ts
+++ tests-playwright/example.spec.ts
@@ -1,20 +1,14 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('homepage has title and links to intro page', async ({ page }) => {
-  await page.goto('/');
+test("homepage has title and links to intro page", async ({ page }) => {
+	await page.goto("/?article=Dark%20Vador");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Welcome to Astro./);
+	await page.screenshot({ path: "example.png" });
 
-  // create a locator
-  const getStarted = page.getByRole('link', { name: 'Documentation' });
-
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', 'https://docs.astro.build/');
-
-  // Click the get started link.
-  await getStarted.click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*build/);
+	// create a locator
+	const locator = page.locator('input[type="text"]');
+	// get the value of the locator
+	const value = await locator.inputValue();
+	// assert the value
+	expect(value).toBe("Dark Vador");
 });
