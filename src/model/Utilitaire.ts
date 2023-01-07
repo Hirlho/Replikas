f9aa2d9c7e9e809c09ba3e23a53c942f429429ca
Champeix Cédric <champeic@debian-BULLSEYE-live-builder-AMD64>
2023-01-07 18:37:26 +0100

    Création de la page auction.astro qui a besoin d'une modification de son style et création de Utilitaire.ts qui contient la fonction dateDiff qui calcul permet de savoir la différence de temps entre 2 dates

---
+++ src/model/Utilitaire.ts
@@ -0,0 +1,33 @@
+export { dateDiff };
+
+/**
+ *
+ * @param date1
+ * @param date2
+ * @returns An object containing {day, hour, min, sec}
+ */
+function dateDiff(date1: Date, date2: Date) {
+  // This function calculates the time between date1 and date2
+
+  var diff = {
+    sec: 0,
+    min: 0,
+    hour: 0,
+    day: 0,
+  };
+  var diff_temps = date1.getTime() - date2.getTime();
+
+  diff_temps = Math.floor(diff_temps / 1000); // Nombre de secondes entre les 2 dates
+  diff.sec = diff_temps % 60; // Extraction du nombre de secondes
+
+  diff_temps = Math.floor((diff_temps - diff.sec) / 60); // Nombre de minutes (partie entière)
+  diff.min = diff_temps % 60; // Extraction du nombre de minutes
+
+  diff_temps = Math.floor((diff_temps - diff.min) / 60); // Nombre d'heures (entières)
+  diff.hour = diff_temps % 24; // Extraction du nombre d'heures
+
+  diff_temps = Math.floor((diff_temps - diff.hour) / 24); // Nombre de jours restants
+  diff.day = diff_temps;
+
+  return diff;
+}
