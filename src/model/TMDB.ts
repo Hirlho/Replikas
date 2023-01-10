488cd37704edd901bffd0ef54a6d8c5240c4b4bb
GaspardCulis <gaspard.culis@gmail.com>
2023-01-10 22:22:12 +0100

    Ajouté options de taille d'image pour le poster

--- src/model/TMDB.ts
+++ src/model/TMDB.ts
@@ -61,10 +61,20 @@ export default class TMDB {
 	 * @param id The movie ID
 	 * @returns Absolute URL to the movie poster or empty string if no poster is available
 	 */
-	public static async getMoviePosterURL(id: number): Promise<string> {
+	public static async getMoviePosterURL(
+		id: number,
+		size:
+			| 'w92'
+			| 'w154'
+			| 'w185'
+			| 'w342'
+			| 'w500'
+			| 'w780'
+			| 'original' = 'original'
+	): Promise<string> {
 		const movie = await this.getMovie(id);
 		return movie.poster_path
-			? `${this._imageBaseUrl}/original${movie.poster_path}`
+			? `${this._imageBaseUrl}/${size}${movie.poster_path}`
 			: '';
 	}
 
