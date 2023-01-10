f359896f0977d97be72e2b8e37185a807eb79b10
GaspardCulis <gaspard.culis@gmail.com>
2023-01-10 11:13:59 +0100

    Maj et fix erreur syntaxe

--- sql/drop.sql
+++ sql/drop.sql
@@ -2,7 +2,7 @@ DROP TABLE aquired;
 DROP TABLE interests;
 DROP TABLE bid;
 DROP TABLE article_image;
-DROP TABLE film;
+DROP TABLE movie;
 DROP TABLE article;
 DROP TABLE company;
 DROP TABLE session;
