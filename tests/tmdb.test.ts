ca67683725ae48a157f2f79e748958de31dbecf5
GaspardCulis <gaspard.culis@gmail.com>
2023-01-10 21:02:26 +0100

    Rajouté des tests en conséquence

--- tests/tmdb.test.ts
+++ tests/tmdb.test.ts
@@ -1,6 +1,6 @@
-import TMDB from '../src/model/TMDB';
+import TMDB, { MovieNotFoundError } from '../src/model/TMDB';
 
-test('TMDB API works fine', async () => {
+test('Can fetch movie from id', async () => {
 	const data = await TMDB.getMovie(550);
 	expect(data).toHaveProperty('original_title', 'Fight Club'); // Check if the movie is Fight Club
 
@@ -8,4 +8,8 @@ test('TMDB API works fine', async () => {
 	expect(posterURL).toBe(
 		'https://image.tmdb.org/t/p/original/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg'
 	);
+});
+
+test('Unknown movie id throws error', async () => {
+	await expect(TMDB.getMovie(0)).rejects.toThrowError(MovieNotFoundError);
 });
