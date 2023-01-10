8d6e0f4d81d7f5ae897e5e22207e1afebf49dffd
GaspardCulis <gaspard.culis@gmail.com>
2023-01-10 10:05:25 +0100

    Added search api route

---
+++ src/pages/api/search.ts
@@ -0,0 +1,26 @@
+import { APIRoute } from "astro";
+import Article from "../../model/Article";
+
+export const get: APIRoute = async ({ request }) => {
+	// Get URL query parameters
+	const url = new URL(request.url);
+	const query = Object.fromEntries(url.searchParams);
+
+	if (!query.search) {
+		return new Response("Missing search query", {
+			status: 400,
+		});
+	}
+
+	const t0 = performance.now();
+	const matches = await Article.getBySearch(query.search);
+	const t1 = performance.now();
+	console.info(`Search took ${t1 - t0} milliseconds.`);
+
+	return new Response(JSON.stringify(matches), {
+		headers: {
+			"content-type": "application/json",
+		},
+		status: 200,
+	});
+};
