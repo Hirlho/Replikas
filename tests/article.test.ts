ae1f8183de3a79b2ff0823f5bfd890c65a5cb21d
GaspardCulis <gaspard.culis@gmail.com>
2023-01-10 17:42:46 +0100

    Rendu la page d'auction fonctionnelle

--- tests/article.test.ts
+++ tests/article.test.ts
@@ -1,60 +1,62 @@
-import Article from "../src/model/Article";
-import Database from "../src/model/Database";
+import Article from '../src/model/Article';
+import Database from '../src/model/Database';
 
-test("Crée un article et lui assigne un id automatiquement", async () => {
-	const article = await Article.create(
-		"Sabre laser",
-		"Un sabre laser de la marque Jedi, utilisé par Darth Vader dans l'épisode 4 de la saga Star Wars. @test-product",
-		1000,
-		1,
-		new Date("2020-01-01"),
-		new Date("2020-01-02"),
-		[],
-		1
-	);
-	expect(article.getId()).toBeGreaterThan(0);
+test('Crée un article et lui assigne un id automatiquement', async () => {
+    const article = await Article.create(
+        'Sabre laser',
+        "Un sabre laser de la marque Jedi, utilisé par Darth Vader dans l'épisode 4 de la saga Star Wars. @test-product",
+        1000,
+        1,
+        new Date('2020-01-01'),
+        new Date('2020-01-02'),
+        [],
+        1,
+        1
+    );
+    expect(article.getId()).toBeGreaterThan(0);
 });
 
-test("Récupère un article existant", async () => {
-	const article = await Article.create(
-		"Sabre laser",
-		"Un sabre laser de la marque Jedi, utilisé par maître Yoda dans l'épisode 4 de la saga Star Wars. @test-product",
-		1000,
-		1,
-		new Date("2020-01-01"),
-		new Date("2020-01-02"),
-		[],
-		1
-	);
-	const article2 = await Article.get(article.getId());
-	expect(article2.getId()).toBe(article.getId());
+test('Récupère un article existant', async () => {
+    const article = await Article.create(
+        'Sabre laser',
+        "Un sabre laser de la marque Jedi, utilisé par maître Yoda dans l'épisode 4 de la saga Star Wars. @test-product",
+        1000,
+        1,
+        new Date('2020-01-01'),
+        new Date('2020-01-02'),
+        [],
+        1,
+        1
+    );
+    const article2 = await Article.get(article.getId());
+    expect(article2.getId()).toBe(article.getId());
 });
 
-test("Cherche des article", async () => {
-	const resultats = await Article.getBySearch("sabre");
-	expect(resultats.length).toBeGreaterThanOrEqual(2);
+test('Cherche des article', async () => {
+    const resultats = await Article.getBySearch('sabre');
+    expect(resultats.length).toBeGreaterThanOrEqual(2);
 
-	const resultats2 = await Article.getBySearch("star wars");
-	expect(resultats2.length).toBeGreaterThanOrEqual(2);
+    const resultats2 = await Article.getBySearch('star wars');
+    expect(resultats2.length).toBeGreaterThanOrEqual(2);
 
-	const resultats3 = await Article.getBySearch("yoda");
-	expect(resultats3.length).toBeGreaterThanOrEqual(1);
+    const resultats3 = await Article.getBySearch('yoda');
+    expect(resultats3.length).toBeGreaterThanOrEqual(1);
 });
 
-test("Cherche des article avec bon ordre de pertinence", async () => {
-	const resultats = await Article.getBySearch("sabre yoda");
-	expect(resultats[0].getDescription()).toContain("Yoda");
-	expect(resultats[1].getDescription()).toContain("Vader");
+test('Cherche des article avec bon ordre de pertinence', async () => {
+    const resultats = await Article.getBySearch('sabre Yoda');
+    expect(resultats[0].getDescription()).toContain('Yoda');
+    expect(resultats[1].getDescription()).toContain('Vader');
 
-	const resultats2 = await Article.getBySearch("sabre star wars vader");
-	expect(resultats2[0].getDescription()).toContain("Vader");
-	expect(resultats2[1].getDescription()).toContain("Yoda");
+    const resultats2 = await Article.getBySearch('sabre star wars Vader');
+    expect(resultats2[0].getDescription()).toContain('Vader');
+    expect(resultats2[1].getDescription()).toContain('Yoda');
 });
 
 afterAll(async () => {
-	const articles_crees = await Article.getBySearch("@test-product");
-	for (const article of articles_crees) {
-		await article.delete();
-	}
-	await Database.close();
+    const articles_crees = await Article.getBySearch('@test-product');
+    for (const article of articles_crees) {
+        await article.delete();
+    }
+    await Database.close();
 });
