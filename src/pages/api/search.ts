386c1b00da29d13f032a5daf805155182a1ee074
GaspardCulis <gaspard.culis@gmail.com>
2023-01-10 10:07:52 +0100

    Ajouté parametres limit et offset à l'api search

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -4,16 +4,21 @@ import Article from "../../model/Article";
 export const get: APIRoute = async ({ request }) => {
 	// Get URL query parameters
 	const url = new URL(request.url);
-	const query = Object.fromEntries(url.searchParams);
+	const query: { [k: string]: any } = Object.fromEntries(url.searchParams);
 
 	if (!query.search) {
 		return new Response("Missing search query", {
 			status: 400,
 		});
 	}
+	query.limit = query.limit ? parseInt(query.limit) : 10;
+	query.offset = query.offset ? parseInt(query.offset) : 0;
 
 	const t0 = performance.now();
-	const matches = await Article.getBySearch(query.search);
+	const matches = await Article.getBySearch(query.search, {
+		limit: query.limit,
+		offset: query.offset,
+	});
 	const t1 = performance.now();
 	console.info(`Search took ${t1 - t0} milliseconds.`);
 
